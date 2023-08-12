a588a51492ac150c802e8c10d826f91a172b837b
Edmund Rosewright <edmundrosewright@gmail.com>
2023-08-12 20:53:10 +0900

    add NewFileScanner and orDie utils

--- 2019/golang/day01/main.go
+++ 2019/golang/day01/main.go
@@ -8,6 +8,24 @@ import (
 	"strconv"
 )
 
+func orDie[T any](val T, err error) T {
+	if err != nil {
+		log.Fatal(err)
+	}
+	return val
+}
+
+// NewFileScanner opens a file and
+// returns an associated *bufio.Scanner
+// as well as a func to close the file.
+//
+//	scanner, close := NewFileScanner("day01/input.txt")
+//	defer close()
+func NewFileScanner(inputPath string) (scanner *bufio.Scanner, close func() error) {
+	file := orDie(os.Open(inputPath))
+	return bufio.NewScanner(file), file.Close
+}
+
 func requiredFuel(mass int) int {
 	fuelMass := mass/3 - 2
 	if fuelMass <= 0 {
@@ -17,25 +35,14 @@ func requiredFuel(mass int) int {
 }
 
 func main() {
-	inputPath := "day01/input.txt"
-
-	file, err := os.Open(inputPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+	scanner, close := NewFileScanner("day01/input.txt")
+	defer close()
 
 	totalFuel := 0
 
-	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		inputTxt := scanner.Text()
-		moduleMass, err := strconv.Atoi(inputTxt)
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
-
+		moduleMass := orDie(strconv.Atoi(inputTxt))
 		totalFuel += requiredFuel(moduleMass)
 	}
 
