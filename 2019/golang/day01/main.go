2dcacffe19d13ad1c3d570398b907f1f237f67b9
Edmund Rosewright <edmundrosewright@gmail.com>
2023-08-12 17:19:49 +0900

    2019 day 1 pt 1

--- 2019/golang/day01/main.go
+++ 2019/golang/day01/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func requiredFuel(moduleMass int) int {
+	return moduleMass/3 - 2
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	inputPath := "day01/input.txt"
+
+	file, err := os.Open(inputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	totalFuel := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		inputTxt := scanner.Text()
+		moduleMass, err := strconv.Atoi(inputTxt)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		moduleFuel := requiredFuel(moduleMass)
+		totalFuel += moduleFuel
+	}
+
+	fmt.Println(totalFuel)
 }
