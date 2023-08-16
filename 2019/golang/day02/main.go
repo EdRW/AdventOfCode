69720fe54c8e433bb9dd56e5e87afe2eef0b8a04
Edmund Rosewright <edmundrosewright@gmail.com>
2023-08-16 13:04:09 +0900

    refactor: 2019 golang day 2 pt 2
    move machine into its own package

--- 2019/golang/day02/main.go
+++ 2019/golang/day02/main.go
@@ -1,97 +1,14 @@
 package main
 
 import (
+	"aoc/day02/machine"
+	"aoc/utils"
 	"fmt"
 	"log"
 	"strings"
-
-	"aoc/utils"
 )
-
-type OpCode int
-
-const (
-	Add OpCode = iota + 1
-	Multiply
-	Halt OpCode = 99
-)
-
-func add(param1 int, param2 int) int {
-	return param1 + param2
-}
-
-func multiply(param1 int, param2 int) int {
-	return param1 * param2
-}
-
-var OpFunc = map[OpCode]func(int, int) int{
-	Add:      add,
-	Multiply: multiply,
-}
-
-func (m *Machine) runOp(opCode OpCode, pointer1 int, pointer2 int, outPointer int) {
-	op, ok := OpFunc[opCode]
-	if !ok {
-		log.Fatalf("Unsupported command: %d", int(opCode))
-	}
-	param1, param2 := m.deref(m.pointer+1), m.deref(m.pointer+2)
-	output := op(param1, param2)
-	fmt.Printf("instruction: %d[%d](%d[%d]:%d[%d]) => %d[%d]\n",
-		opCode, m.pointer, param1, m.pointer+1, param2, m.pointer+2, output, m.pointer+3)
-
-	m.assignValue(m.pointer+3, output)
-}
-
-type Machine struct {
-	pointer  int
-	size     int
-	intCodes []int
-	output   int
-}
-
-func NewMachine(size int, intCodes []int) *Machine {
-	return &Machine{
-		pointer:  0,
-		size:     size,
-		intCodes: intCodes,
-	}
-}
-
-func (m *Machine) deref(pointer int) int {
-	return m.intCodes[m.intCodes[pointer]]
-}
-
-func (m *Machine) assignValue(pointer int, value int) {
-	m.intCodes[m.intCodes[pointer]] = value
-}
-
-// advance instruction pointer
-func (m *Machine) advanceIntCodePointer() {
-	m.pointer += m.size
-}
-
-func (m *Machine) process() bool {
-	opCode := OpCode(m.intCodes[m.pointer])
-	if opCode == Halt {
-		m.output = m.intCodes[0]
-		fmt.Println("Halted!")
-		return false
-	}
-
-	m.runOp(opCode, m.pointer+1, m.pointer+2, m.pointer+3)
-
-	m.advanceIntCodePointer()
-
-	return true
-}
-
-func (m *Machine) Run() {
-	for m.process() {
-	}
-}
 
 func getIntCodes() []int {
-
 	input := utils.AocInputFile(2)
 	scanner, close := utils.NewFileScanner(input)
 	if !scanner.Scan() {
@@ -102,20 +19,51 @@ func getIntCodes() []int {
 	inputTxt := scanner.Text()
 	intCodeStrs := strings.Split(inputTxt, ",")
 	return utils.ToInts(intCodeStrs)
+}
 
+func initIntCodes(intCodes []int, noun int, verb int) {
+	intCodes[1] = noun
+	intCodes[2] = verb
+}
+
+func bruteForceInputs(computer *machine.Machine, intCodes []int, goal int) (int, int) {
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 100; j++ {
+			// Restoring the gravity assist program inputs
+			initIntCodes(intCodes, i, j)
+
+			output := computer.Run(intCodes)
+			if output == goal {
+				return i, j
+			}
+		}
+	}
+	log.Fatalf("unable to achieve goal %d\n", goal)
+	return -1, -1
 }
 
 func main() {
 	intCodes := getIntCodes()
 
-	// Restoring the gravity assist program inputs
-	intCodes[1] = 12
-	intCodes[2] = 2
-	fmt.Printf("intCodes: %d\n", intCodes)
-
 	instructionSize := 4
-	machine := NewMachine(instructionSize, intCodes)
-	machine.Run()
+	computer := machine.NewMachine(instructionSize)
 
-	fmt.Printf("result: %d\n", machine.output)
+	// part 1
+	initIntCodes(intCodes, 12, 2)
+	p1Output := computer.Run(intCodes)
+	fmt.Printf("part 1 result: %d\n", p1Output)
+
+	// part 2
+	goal := 19690720
+	noun, verb := bruteForceInputs(computer, intCodes, goal)
+
+	// verify noun and verb
+	initIntCodes(intCodes, noun, verb)
+	if computer.Run(intCodes) != goal {
+		log.Fatalf("Could not verify that noun: %d, verb: %d produce goal: %d\n", noun, verb, goal)
+	}
+
+	p2Output := 100*noun + verb
+
+	fmt.Printf("part 2 result: %d\n", p2Output)
 }
