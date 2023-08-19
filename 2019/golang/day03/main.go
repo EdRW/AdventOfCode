546e42065c569cb02b45fd9bb534b43e1d0e8034
Edmund Rosewright <edmundrosewright@gmail.com>
2023-08-19 21:11:13 +0900

    style: combine parts 1 and 2 in main

--- 2019/golang/day03/main.go
+++ 2019/golang/day03/main.go
@@ -15,54 +15,6 @@ func NewPoint(x int, y int) Point {
 	return Point{x, y}
 }
 
-type WirePath struct {
-	set utils.Set[Point]
-}
-
-func (w WirePath) Add(point Point) {
-	w.set.Add(point)
-}
-
-func (w WirePath) Exists(point Point) bool {
-	return w.set.Exists(point)
-}
-
-func (w WirePath) Range() utils.Set[Point] {
-	return w.set
-}
-
-func NewWirePath() WirePath {
-	return WirePath{utils.NewSet[Point]()}
-}
-
-func getWirePath(pathString []string) WirePath {
-	wirePath := NewWirePath()
-
-	xyCoords := NewPoint(0, 0)
-
-	for _, event := range pathString {
-		direction := event[0]
-		magnitude := utils.ToInt(event[1:])
-
-		sign := 1
-		if direction == 'L' || direction == 'D' {
-			sign = -1
-		}
-
-		for i := 0; i < magnitude; i++ {
-			if direction == 'L' || direction == 'R' {
-				xyCoords.X += sign
-			} else {
-
-				xyCoords.Y += sign
-			}
-			wirePath.Add(NewPoint(xyCoords.X, xyCoords.Y))
-		}
-	}
-
-	return wirePath
-}
-
 type WirePathSteps map[Point]int
 
 func getWirePathSteps(pathString []string) WirePathSteps {
@@ -113,46 +65,7 @@ func stepDistance(point Point,
 	return stepsA + stepsB
 }
 
-func part1() {
-	input := utils.AocInputFile(3)
-	scanner, close := utils.NewFileScanner(input)
-	defer close()
-
-	wirePaths := make([]WirePath, 2)
-	for i := 0; scanner.Scan(); i++ {
-		inputTxt := scanner.Text()
-		pathString := strings.Split(inputTxt, ",")
-		wirePaths[i] = getWirePath(pathString)
-	}
-
-	wirePath1 := wirePaths[0]
-	wirePath2 := wirePaths[1]
-
-	intersections := make([]Point, 0)
-
-	for k := range wirePath1.Range() {
-		if ok := wirePath2.Exists(k); ok {
-			intersections = append(intersections, k)
-		}
-	}
-
-	if len(intersections) == 0 {
-		fmt.Println("The paths don't cross!")
-		return
-	}
-
-	leastDistance := mhtnDistance(intersections[0])
-	for _, intersection := range intersections[1:] {
-		distance := mhtnDistance(intersection)
-		if distance < leastDistance {
-			leastDistance = distance
-		}
-	}
-
-	fmt.Printf("Part 1: closest intersection is %d blocks away!\n", leastDistance)
-}
-
-func part2() {
+func main() {
 	input := utils.AocInputFile(3)
 	scanner, close := utils.NewFileScanner(input)
 	defer close()
@@ -180,18 +93,23 @@ func part2() {
 		return
 	}
 
-	leastSteps := stepDistance(intersections[0], wirePath1, wirePath2)
+	firstIntersection := intersections[0]
+
+	leastDistance := mhtnDistance(firstIntersection)
+	leastSteps := stepDistance(firstIntersection, wirePath1, wirePath2)
+
 	for _, intersection := range intersections[1:] {
+		distance := mhtnDistance(intersection)
+		if distance < leastDistance {
+			leastDistance = distance
+		}
+
 		stepDistance := stepDistance(intersection, wirePath1, wirePath2)
 		if stepDistance < leastSteps {
 			leastSteps = stepDistance
 		}
 	}
 
+	fmt.Printf("Part 1: closest intersection is %d blocks away!\n", leastDistance)
 	fmt.Printf("Part 2: least steps to intersection is %d steps away!\n", leastSteps)
-}
-
-func main() {
-	part1()
-	part2()
 }
