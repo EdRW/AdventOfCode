ee4c8fea533d4877f55dff2343fe25c18838ebbc
Edmund Rosewright <edmundrosewright@gmail.com>
2023-09-15 20:03:21 +0900

    add Queue implementation based on channels

---
+++ 2019/golang/utils/queue.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+	"errors"
+)
+
+// Queue is a slice with FIFO added operations
+type Queue[T comparable] chan T
+
+func NewBufferedQueue[T comparable](capacity int) Queue[T] {
+	return make(Queue[T], capacity)
+}
+
+// Add a value to the end of the queue
+// and block until queue capacity is available
+func (ch Queue[T]) BlockingEnqueue(value T) {
+	ch <- value
+}
+
+// Add a value to the end of the queue
+// and return an error if queue capacity
+// is would be exceeded
+func (ch Queue[T]) Enqueue(value T) error {
+	if len(ch) >= cap(ch) {
+		return errors.New("Queue capacity exceeded")
+	}
+	ch.BlockingEnqueue(value)
+	return nil
+}
+
+// Remove and return the value at the front of the queue
+// and block until a value is available
+func (q *Queue[T]) BlockingDequeue() T {
+	return <-*q
+}
+
+// Remove and return the value at the front of the queue
+// and return an error if the queue is empty
+func (ch Queue[T]) Dequeue() (T, error) {
+
+	if len(ch) == 0 {
+		var value T
+		return value, errors.New("Dequeue called on empty queue")
+	}
+
+	return ch.BlockingDequeue(), nil
+}
