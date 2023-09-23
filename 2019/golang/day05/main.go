3c0b6c554cb3bdd58fc9f99043d3d584a9482345
Edmund Rosewright <edmundrosewright@gmail.com>
2023-09-23 22:07:22 +0900

    fix cli prompts for day 5

--- 2019/golang/day05/main.go
+++ 2019/golang/day05/main.go
@@ -10,11 +10,20 @@ func main() {
 	inputPath := utils.AOCInputFile(5)
 	intCodes := machine.GetIntCodesFromFile(inputPath)
 
+	// configure the computer
+	options := machine.Options{
+		IO: &machine.IO{
+			StdIn:  machine.NewUserInputReader("Enter the System ID: "),
+			StdOut: machine.NewUserOutputWriter("Output: "),
+		},
+	}
+	computer := machine.NewComputer(options)
+
 	// part 1
 	fmt.Println("~~~~~ Part 1 ~~~~~")
 	fmt.Println("HINT: Enter '1' for System ID")
-	computer := machine.NewComputer()
 	computer.Run(intCodes)
+	fmt.Println("expected:", 13547311)
 
 	fmt.Println()
 
@@ -22,4 +31,5 @@ func main() {
 	fmt.Println("~~~~~ Part 2 ~~~~~")
 	fmt.Println("HINT: Enter '5' for System ID")
 	computer.Run(intCodes)
+	fmt.Println("expected:", 236453)
 }
